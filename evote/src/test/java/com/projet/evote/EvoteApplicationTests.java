7b88993342b16c1be41699a57439a79449bddc6c
Kader <73432511+GitKad-er@users.noreply.github.com>
2022-08-05 13:00:51 +0000

    Add files via upload

---
+++ evote/src/test/java/com/projet/evote/EvoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.evote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
